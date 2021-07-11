bb431290ede75f3b7686a7fdcd32790ff75d4c86
frankyu12 <75060875+frankyu12@users.noreply.github.com>
2021-07-12 05:00:35 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const downIcon  = document.querySelector('#question-active > div > div.icon-up > p > i');
+const downIcon  = document.querySelector('#question-active > div:nth-child(1) > div.icon-up');
 const hiddenText = document.querySelector('#answer-text');
+const question =document.querySelector('#question-active > div:nth-child(1) > span > p');
+const upBtn =document.querySelector('.icon-down');
+const upBtnNone =document.querySelector("#answer-text > p");
 
-
-downIcon.addEventListener('click', () => {
+question.addEventListener('click', () => {
     hiddenText.classList.remove('hidden');
     hiddenText.classList.add('.hiidentextStyle')
+    downIcon.classList.add('.hidden')
+    upBtn.classList.remove('hidden')
 });
+upBtnNone.addEventListener('click', () => {
+    upBtn.classList.add('hidden')
+    hiddenText.classList.add('hidden')
+
+})
 
 
